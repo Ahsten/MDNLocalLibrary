5fcc5db0a7946e758b25c0179793381d7833f109
Luxe17 <hanson1717@live.com>
2022-01-20 19:02:47 -0700

    Connect MongoDB and add models

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -4,6 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+var mongoDB = "mongodb+srv://Deft:library@cluster0.pieq2.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
